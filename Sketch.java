c2d3349b499e7e7fa14d297855d78ce751cf385b
eattai <ethan.singsonanca@gmail.com>
2023-12-15 00:29:23 -0500

    finished

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,108 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
+
+  PImage imgSky;
+  PImage imgJet;
+  PImage imgNightSky;
+
+  float jetX;
+  float jetY;
+  float jetZ;
 	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
+  
   public void settings() {
-	// put your size call here
-    size(400, 400);
+	
+    size(600, 600);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+  
   public void setup() {
-    background(210, 255, 173);
+
+   imgSky = loadImage("sky.jpg");
+   imgSky.resize(imgSky.width/4, imgSky.height/5);
+
+   imgNightSky = loadImage("skynight.jpg");
+
+   imgJet = loadImage("jet.png");
+   imgJet.resize(imgJet.width/4, imgJet.height/4); // Resize the jet image
+
+   jetX = 250;
+   jetY = 400;
+   jetZ = 50;
+}
+    
+  
+
+ 
+public void draw() {
+  background(0);
+
+  image(imgSky, 0, 0); // Display the sky image
+
+  if (keyPressed) {
+      if (key == 'f') {
+          image(imgNightSky, 0, 0); // Display the night sky image if 'f' is pressed
+      }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+ 
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  image(imgJet, jetX, jetY); // Display the jet image at the specified coordinates
+  
+  if(dist(mouseX, mouseY, jetX, jetY) < jetZ) {
+
+    if(mousePressed){
+      jetX = mouseX;
+      jetY = mouseY;
+    }
+  }
+
+  if(upPressed) {
+    jetY--;
+  }
+
+  if(downPressed) {
+    jetY++;
+  }
+
+  if(leftPressed) {
+    jetX--;
   }
   
-  // define other methods down here.
+  if(rightPressed) {
+    jetX++;
+    }
+  }
+
+  public void keyPressed() {
+    if (keyCode == 'W') {
+        upPressed = true;
+    } else if (keyCode == 'S') {
+        downPressed = true;
+    } else if (keyCode == 'A') {
+        leftPressed = true;
+    } else if (keyCode == 'D') {
+        rightPressed = true;
+    }
+}
+
+public void keyReleased() {
+    if (keyCode == 'W') {
+        upPressed = false;
+    } else if (keyCode == 'S') {
+        downPressed = false;
+    } else if (keyCode == 'A') {
+        leftPressed = false;
+    } else if (keyCode == 'D') {
+        rightPressed = false;
+    }
+}
+
 }
